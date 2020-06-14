2ca49686bcd3be4053858c77e33adc214c745a80
makutak <takkouno@gmail.com>
2020-06-14 15:53:51 +0900

    円を描く

--- src/cljs_breakout/core.cljs
+++ src/cljs_breakout/core.cljs
@@ -3,14 +3,28 @@
 
 (enable-console-print!)
 
+(def canvas)
+(def ctx)
+
+(defn create-square []
+  (.beginPath ctx)
+  (.rect ctx 20 40 50 50)
+  (aset ctx "fillStyle" "#FF0000")
+  (.fill ctx)
+  (.closePath ctx))
+
+(defn create-circle []
+  (.beginPath ctx)
+  (.arc ctx 240 160 20 0 (* Math.PI 2) false)
+  (aset ctx "fillStyle" "green")
+  (.fill ctx)
+  (.closePath ctx))
+
 (defn init []
-  (let [canvas (.getElementById js/document "my-canvas")
-        ctx (.getContext canvas "2d")]
-    (.beginPath ctx)
-    (.rect ctx 20 40 50 50)
-    (aset ctx "fillStyle" "#FF0000")
-    (.fill ctx)
-    (.closePath ctx)))
+  (let [canvas (.getElementById js/document "my-canvas")]
+    (set! ctx (.getContext canvas "2d"))
+    (create-square)
+    (create-circle)))
+
 
 (init)
-
