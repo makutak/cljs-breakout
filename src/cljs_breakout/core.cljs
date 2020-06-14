5b3eba256c02dac37cadc523a1fdcd2ac3473d95
makutak <takkouno@gmail.com>
2020-06-14 16:25:19 +0900

    10ミリ秒おきにボールを描画する

--- src/cljs_breakout/core.cljs
+++ src/cljs_breakout/core.cljs
@@ -3,35 +3,14 @@
 
 (enable-console-print!)
 
-(def canvas)
-(def ctx)
+(def canvas (.getElementById js/document "my-canvas"))
+(def ctx (.getContext canvas "2d"))
 
-(defn create-square []
+(defn draw []
   (.beginPath ctx)
-  (.rect ctx 20 40 50 50)
-  (aset ctx "fillStyle" "#FF0000")
+  (.arc ctx 50 50 10 0 (* Math.PI 2))
+  (aset ctx "fillStyle" "#0095DD")
   (.fill ctx)
   (.closePath ctx))
 
-(defn create-circle []
-  (.beginPath ctx)
-  (.arc ctx 240 160 20 0 (* Math.PI 2) false)
-  (aset ctx "fillStyle" "green")
-  (.fill ctx)
-  (.closePath ctx))
-
-(defn create-stroke-square []
-  (.beginPath ctx)
-  (.rect ctx 160 10 100 40)
-  (aset ctx "strokeStyle" "rgba(0, 0, 255, 0.5)")
-  (.stroke ctx)
-  (.closePath ctx))
-
-(defn init []
-  (let [canvas (.getElementById js/document "my-canvas")]
-    (set! ctx (.getContext canvas "2d"))
-    (create-square)
-    (create-circle)
-    (create-stroke-square)))
-
-(init)
+(js/setInterval draw 10)
