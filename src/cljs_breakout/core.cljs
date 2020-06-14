2e0bf4280580f841cc17c99cb1a196bbac1b9f79
makutak <takkouno@gmail.com>
2020-06-14 16:29:35 +0900

    ボールが移動するが軌跡が残る

--- src/cljs_breakout/core.cljs
+++ src/cljs_breakout/core.cljs
@@ -6,11 +6,19 @@
 (def canvas (.getElementById js/document "my-canvas"))
 (def ctx (.getContext canvas "2d"))
 
+(def x (/ (. canvas -width) 2))
+(def y (- (. canvas -height) 30))
+
+(def dx 2)
+(def dy -2)
+
 (defn draw []
   (.beginPath ctx)
-  (.arc ctx 50 50 10 0 (* Math.PI 2))
+  (.arc ctx x y 10 0 (* Math.PI 2))
   (aset ctx "fillStyle" "#0095DD")
   (.fill ctx)
-  (.closePath ctx))
+  (.closePath ctx)
+  (set! x (+ x dx))
+  (set! y (+ y dy)))
 
 (js/setInterval draw 10)
