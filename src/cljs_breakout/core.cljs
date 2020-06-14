d56bd647e0cd6814d16b28a0b3ee39204b4d8568
makutak <takkouno@gmail.com>
2020-06-14 16:07:42 +0900

    図形をfill()で塗りつぶすかわりにstroke()で縁だけ色を付けることもできる

--- src/cljs_breakout/core.cljs
+++ src/cljs_breakout/core.cljs
@@ -20,11 +20,18 @@
   (.fill ctx)
   (.closePath ctx))
 
+(defn create-stroke-square []
+  (.beginPath ctx)
+  (.rect ctx 160 10 100 40)
+  (aset ctx "strokeStyle" "rgba(0, 0, 255, 0.5)")
+  (.stroke ctx)
+  (.closePath ctx))
+
 (defn init []
   (let [canvas (.getElementById js/document "my-canvas")]
     (set! ctx (.getContext canvas "2d"))
     (create-square)
-    (create-circle)))
-
+    (create-circle)
+    (create-stroke-square)))
 
 (init)
